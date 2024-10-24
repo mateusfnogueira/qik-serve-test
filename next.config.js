2ab19480131fae3e8e15888f8b3b180aef86619b
mateusfnogueira <mateusfnogueira@outlook.com>
2024-10-24 17:14:51 -0300

    feat(config): set to deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextIntl = require("next-intl/plugin")();
+const nextIntl = require('next-intl/plugin')()
 const nextConfig = nextIntl({
+  basePath: '/qik-serve-test',
+  output: 'export',
+  reactStrictMode: true,
   images: {
-    domains: ["preodemo.gumlet.io"],
-  },
-});
+    domains: ['preodemo.gumlet.io']
+  }
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
